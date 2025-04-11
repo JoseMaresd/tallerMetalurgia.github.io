e65aefca2c0fb1727ac1748d12a01bd40c91de69
Jose Juan Mares <138920712+JoseMaresd@users.noreply.github.com>
2025-04-11 10:50:25 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // For username.github.io repositories, the base should be '/'
+  base: "/",
 })
